80d6cf9e414abf016dc9918a4d39f0958eeed0b2
fakynno <fakynno@gmail.com>
2022-04-02 11:09:25 -0300

    Project initial import

---
+++ expenses-control-api/src/main/java/br/com/fakynno/expensescontrolapi/ExpensesControlApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fakynno.expensescontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesControlApiApplication.class, args);
+	}
+
+}
---
+++ expenses-control-api/src/test/java/br/com/fakynno/expensescontrolapi/ExpensesControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fakynno.expensescontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
